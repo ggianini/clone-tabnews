b32296689f8b83f0a6cb7c2aebd8e82dd13688e8
Gustavo Gianini <gustavoogian@gmail.com>
2024-07-18 10:24:07 -0300

    faz o `database.js` mais robusto para tratar erros

--- infra/database.js
+++ infra/database.js
@@ -9,6 +9,14 @@ async function query(queryObject) {
     password: process.env.PGPASSWORD,
   });
 
+  console.log("Variáveis de ambiente do Postgres:", {
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
+    user: process.env.POSTGRES_USER,
+    database: process.env.POSTGRES_DB,
+    password: process.env.PGPASSWORD,
+  });
+
   try {
     await client.connect();
     const result = await client.query(queryObject);
