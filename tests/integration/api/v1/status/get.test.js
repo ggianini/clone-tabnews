6cb995a7f2fb81f35ee9f340389696c6ff3e2884
Gustavo Gianini <gustavoogian@gmail.com>
2024-07-23 21:50:23 -0300

    adicionando ssl para prod

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -7,7 +7,7 @@ test("GET to /api/vi/status", async () => {
   const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
   expect(responseBody.updated_at).toEqual(parsedUpdatedAt);
 
-  expect(responseBody.dependencies.database.version).toEqual("16.0");
-  expect(responseBody.dependencies.database.max_connections).toEqual(100);
+  expect(responseBody.dependencies.database.version).toEqual("16.3");
+  expect(responseBody.dependencies.database.max_connections).toEqual(112);
   expect(responseBody.dependencies.database.opened_connections).toEqual(1);
 });
